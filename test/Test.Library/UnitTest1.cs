fc47cebe86ac916f28053f2f1a249759a31ec12a
floroldos <florencia.roldos@correo.ucu.edu.uy>
2022-09-21 10:44:53 -0300

    tests viejos

--- test/Test.Library/UnitTest1.cs
+++ test/Test.Library/UnitTest1.cs
@@ -1,18 +1,32 @@
 using NUnit.Framework;
 
-namespace Test.Library
+namespace Roleplay_PII_Grupal
 {
-    public class Tests
+
+    public class ExampleTest
     {
-        [SetUp]
-        public void Setup()
+
+        [Test]
+        public void ataqueTest()
         {
+            Item palito = new Item("Palito", "Hola", 100, 50, 0);
+            Magos mago1 = new Magos("Oswald", 100, palito);
+            Magos mago2 = new Magos("Emi", 100, palito);
+            mago1.Atacar(mago2);
+            Assert.AreEqual(mago2.Vida, palito.ValorAtaque);
         }
 
         [Test]
-        public void Test1()
+        public void ataqueNegativo()
         {
-            Assert.Pass();
+            Item palito = new Item("Palito", "Hola", 100, 50, 0);
+            Magos mago1 = new Magos("Oswald", 100, palito);
+            Magos mago2 = new Magos("Emi", 100, palito);
+            mago1.Atacar(mago2);
+            mago1.Atacar(mago2);
+            mago1.Atacar(mago2);
+            Assert.AreEqual(mago2.Vida, 0);
         }
+
     }
 }
