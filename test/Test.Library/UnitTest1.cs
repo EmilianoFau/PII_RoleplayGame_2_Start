c17f53b4b83adbbf6ca3493d11c6ce84d2e8ead6
Renzo Giacometti <renzo.giacometti@gmail.com>
2022-09-23 13:30:02 -0300

    agregada referencia del test

--- test/Test.Library/UnitTest1.cs
+++ test/Test.Library/UnitTest1.cs
@@ -1,6 +1,7 @@
 using NUnit.Framework;
+using RoleplayGame;
 
-namespace Roleplay_PII_Grupal
+namespace Roleplay_PII_Grup00l
 {
 
     public class ExampleTest
@@ -10,8 +11,8 @@ namespace Roleplay_PII_Grupal
         public void ataqueTest()
         {
             Item palito = new Item("Palito", "Hola", 100, 50, 0);
-            Magos mago1 = new Magos("Oswald", 100, palito);
-            Magos mago2 = new Magos("Emi", 100, palito);
+            Wizard mago1 = new Wizard("Oswald", 100, palito);
+            Wizard mago2 = new Wizard("Emi", 100, palito);
             mago1.Atacar(mago2);
             Assert.AreEqual(mago2.Vida, palito.ValorAtaque);
         }
