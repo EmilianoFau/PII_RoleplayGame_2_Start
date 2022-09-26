8fafa9c898a57a4bc18cb131adbebb60c1e91f39
erikhernandez9 <erik.hernandez@correo.ucu.edu.uy>
2022-09-25 21:55:27 -0300

    Test

--- test/Test.Library/UnitTest1.cs
+++ test/Test.Library/UnitTest1.cs
@@ -1,33 +1,50 @@
 using NUnit.Framework;
-using RoleplayGame;
 
-namespace Roleplay_PII_Grup00l
+namespace RoleplayGame
 {
-
-    public class ExampleTest
+    public class Test
     {
-
-        [Test]
-        public void ataqueTest()
+        [SetUp]
+        public void Setup()
         {
-            Item palito = new Item("Palito", "Hola", 100, 50, 0);
-            Wizard mago1 = new Wizard("Oswald", 100, palito);
-            Wizard mago2 = new Wizard("Emi", 100, palito);
-            mago1.Atacar(mago2);
-            Assert.AreEqual(mago2.Vida, palito.ValorAtaque);
         }
 
         [Test]
         public void ataqueNegativo()
         {
-            Item palito = new Item("Palito", "Hola", 100, 50, 0);
-            Magos mago1 = new Magos("Oswald", 100, palito);
-            Magos mago2 = new Magos("Emi", 100, palito);
-            mago1.Atacar(mago2);
-            mago1.Atacar(mago2);
-            mago1.Atacar(mago2);
-            Assert.AreEqual(mago2.Vida, 0);
+            SpellsBook book = new SpellsBook();
+            book.Spells = new Spell[]{ new Spell() };
+
+            Wizard gandalf = new Wizard("Gandalf");
+            gandalf.Staff = new Staff();
+            gandalf.SpellsBook = book;
+
+            Dwarf gimli = new Dwarf("Gimli");
+            gimli.Axe = new Axe();
+            gimli.Helmet = new Helmet();
+            gimli.Shield = new Shield();
+
+            gimli.ReceiveAttack(gandalf.AttackValue);
+            gimli.ReceiveAttack(gandalf.AttackValue);
+
+            Assert.AreEqual(gimli.Health, 0);
         }
 
+        [Test]
+        public void TestAtaque()
+        {
+            Archer gandalf = new Archer("Gandalf");
+            gandalf.Bow = new Bow();
+            gandalf.Helmet = new Helmet();
+
+            Dwarf gimli = new Dwarf("Gimli");
+            gimli.Axe = new Axe();
+            gimli.Helmet = new Helmet();
+            gimli.Shield = new Shield();
+
+            gandalf.ReceiveAttack(gimli.AttackValue);
+
+            Assert.AreEqual(gandalf.Health, 93);
+        }
     }
 }
